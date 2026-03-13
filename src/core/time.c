097339299d1fe6daac533677de1b99f1c1ed8f42
Shabar sha <shabarshas@gmail.com>
2026-03-14 00:13:33 +0530

    update time functions

--- src/core/time.c
+++ src/core/time.c
@@ -3,30 +3,27 @@
 #include "time.h"
 
 static uint64_t last_time = 0;
-static float cached_time = 0;
-static int called_this_frame = 0;
+static float dt = 0.0f;
 
-float deltaTime(){
+void updateTime() {
     uint64_t current_time = SDL_GetTicks();
-    if (!called_this_frame) {
-        float dt = (current_time - last_time) / 1000.0f;
 
-        //clamping the deltatime to 50ms
-        if (dt > 0.05f) dt = 0.05f;
-        if (dt < 0.0f) dt = 0.0f;
-
+    if (last_time == 0) {
         last_time = current_time;
-        cached_time = dt;
-        called_this_frame = 1;
+        return;
     }
 
-    return cached_time;
+    dt = (current_time - last_time) / 1000.0f;
+
+    //capping
+    if (dt > 0.05f) dt = 0.05f;
+    if (dt < 0.0f) dt = 0.0f;
+
+    last_time = current_time;
 }
 
-void updateTime(){
-    uint64_t current_time = SDL_GetTicks();
-    if (last_time == 0) last_time = current_time;
-    called_this_frame = 0;
+float deltaTime() {
+    return dt;
 }
 
 uint64_t getTime(){
@@ -36,7 +33,7 @@ uint64_t getTime(){
 
 void capFPS(uint64_t start_frame){
     uint64_t end_frame = SDL_GetTicks();
-    uint64_t frame_time = end_frame - start_frame;
+    float frame_time = (float)(end_frame - start_frame);
     // this delays the next cycle if the current cycle has ended within FRAME_TICKS
-    if (frame_time < FRAME_TICKS) SDL_Delay(FRAME_TICKS - frame_time); 
+    if (frame_time < FRAME_TICKS) SDL_Delay((uint64_t)FRAME_TICKS - frame_time); 
 }
--- src/game/variables.c
+++ src/game/variables.c
@@ -2,14 +2,14 @@
 
 int game_running = 1;
 
-int player_speed = 200;
+int player_speed = 400;
 int player_health = 100;
 int player_damage = 100;
 int score = 0;
 
 int bullet_speed = 800;
 int bullet_spread = 5;
-int bullet_delay = 100;
+int bullet_delay = 300;
 
 game_mode mode;
 levels level;
